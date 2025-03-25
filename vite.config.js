cf318d32bbff3ef83d01d6623f07940838ff3d2c
Nadezhda Babar <nadezhda.evgenevna000@gmail.com>
2025-03-25 22:22:18 +0500

    add header block

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
